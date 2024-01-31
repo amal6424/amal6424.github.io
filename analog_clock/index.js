ab38f7476dfd5607ce6a706a84967806602e9c19
amal6424 <amalaugustine6425@gmail.com>
2024-01-31 22:21:04 +0530

    update: analog_clock

--- analog_clock/index.js
+++ analog_clock/index.js
@@ -1,11 +1,23 @@
-//let canvas = new HTMLCanvasElement();
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
+const hr_hand = document.querySelector(".hr_hand");
+const min_hand = document.querySelector(".min_hand");
+const sec_hand = document.querySelector(".sec_hand");
+
 main();
 function main(){
-    let cx = canvas.width/2;
-    let cy = canvas.height/2;
-    context.fillStyle = "#ff0000";
-    context.strokeStyle = "#ff0000";
-    context.fillRect(cx,cy, 10,100);
+    setInterval(updateTime, 1000);
+    updateTime();
+}
+function updateTime(){
+    const clock = new Date();
+    let hour = clock.getHours();
+    const min = clock.getMinutes();
+    const sec = clock.getSeconds();
+    const hour_deg_unit = 0.5*min;
+    if(hour> 12) hour -= 12;
+    const hr_deg = (360/12)*hour;
+    const min_deg = (360/60)*min;
+    const sec_deg = (360/60)*sec;
+    hr_hand.style.transform = `rotate(${hr_deg+hour_deg_unit}deg)`;
+    min_hand.style.transform = `rotate(${min_deg}deg)`;
+    sec_hand.style.rotate = `${sec_deg}deg`;
 }
